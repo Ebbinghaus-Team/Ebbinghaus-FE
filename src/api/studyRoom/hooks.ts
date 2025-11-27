e852aa2d0ac8dc2ea9a7b027b59c031c68b65eae
INSANE-P <chanbin0626@gmail.com>
2025-11-27 14:16:34 +0900

    feat #10: 그룹 스터디 생성 뮤테이션 훅(useCreateGroupStudyRoomMutation) 구현

--- src/api/studyRoom/hooks.ts
+++ src/api/studyRoom/hooks.ts
@@ -1,10 +1,12 @@
 import { useMutation } from '@tanstack/react-query';
 import toast from 'react-hot-toast';
-import { createPersonalStudyRoom } from '.';
+import { createPersonalStudyRoom, createGroupStudyRoom } from '.';
 import { showApiErrorToast } from '../../utils/api/showApiErrorToast';
 import type {
   CreatePersonalStudyRoomBody,
   CreatePersonalStudyRoomResponse,
+  CreateGroupStudyRoomBody,
+  CreateGroupStudyRoomResponse,
 } from '../../types/studyRoom';
 import type { ApiError } from '../../types/common';
 
@@ -14,6 +16,15 @@ export const useCreatePersonalStudyRoomMutation = () =>
       createPersonalStudyRoom(createPersonalStudyRoomBody),
     onSuccess: () => {
       toast.success('개인 공부방이 생성되었습니다.');
+    },
+    onError: showApiErrorToast,
+  });
+
+export const useCreateGroupStudyRoomMutation = () =>
+  useMutation<CreateGroupStudyRoomResponse, ApiError, CreateGroupStudyRoomBody>({
+    mutationFn: (createGroupStudyRoomBody) => createGroupStudyRoom(createGroupStudyRoomBody),
+    onSuccess: () => {
+      toast.success('그룹 스터디가 생성되었습니다.');
     },
     onError: showApiErrorToast,
   });
